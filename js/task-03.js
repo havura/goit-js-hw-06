296c24d1db2e1522b13e8e3daa9ee79884863266
Havura Artur <havuraa@gmail.com>
2023-02-10 18:45:34 +0200

    adding tasks 3-5

--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,22 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=720',
     alt: 'White and Black Long Fur Cat',
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=720',
     alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=720',
     alt: 'Group of Horses Running',
   },
 ];
+
+const listGallery = document.querySelector(".gallery");
+
+const createGalleryItem = images.map(({ url, alt }) => `<li><img src= ${url} alt= ${alt}></li>`).join("");
+listGallery.insertAdjacentHTML("afterbegin", createGalleryItem);
+
+console.log(listgGallery);
+console.log(createGalleryItem);
---
+++ js/task-04.js
@@ -0,0 +1,18 @@
+let counterValue = 0;
+
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+const valueEl = document.getElementById('value');
+
+const increment = () => {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+}
+
+const decrement = () => {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
+
+decrementBtn.addEventListener('click', decrement)
+incrementBtn.addEventListener('click', increment)
