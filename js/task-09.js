1b1cadc5d8f25632047b72c5f94e5a4ba6206a54
Havura Artur <havuraa@gmail.com>
2023-02-13 11:29:42 +0200

    adding 9 and 10 tasks

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const buttonEl = document.querySelector(".change-color");
+const spanEl = document.querySelector(".color");
+const bodyEl = document.querySelector("body");
+
+const changeColor = () => {
+  spanEl.textContent = getRandomHexColor();
+  bodyEl.style.backgroundColor = getRandomHexColor();
+};
+buttonEl.addEventListener("click", changeColor);
